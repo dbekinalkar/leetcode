ebfaa04fdb191abacf5b3877aeb227802530a2f1
dbekinalkar <daniyal.bekinalkar@gmail.com>
2025-04-10 04:20:38 -0400

    Update 8 string to integer atoi

--- 8.string-to-integer-atoi.java
+++ 8.string-to-integer-atoi.java
@@ -7,11 +7,6 @@
 // @lc code=start
 class Solution {
     public int myAtoi(String s) {
-        if(s.length() <= 0) {
-            return 0;
-        }
-
-
         int i = 0;
 
         while(i < s.length() && s.charAt(i) == ' ') {
