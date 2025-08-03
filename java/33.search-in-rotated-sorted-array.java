13e83f7efca5a18c0e81665aea8c0301624dd4b3
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2025-08-03 16:31:32 -0500

    33 java

---
+++ java/33.search-in-rotated-sorted-array.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int m;
+
+        while (l <= r) {
+            m = l + (r - l) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            } 
+            else if (nums[m] >= nums[l]) {
+                if (nums[l] <= target && target <= nums[m]) {
+                    r = m - 1;
+                } 
+                else {
+                    l = m + 1;
+                }
+            } 
+            else {
+                if (nums[m] <= target && target <= nums[r]) {
+                    l = m + 1;
+                } 
+                else {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;        
+    }
+}
+// @lc code=end
+
