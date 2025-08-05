400e85ed26a50b23e28d6656c81dd5b806e67625
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2025-08-05 17:49:31 -0500

    199 java

---
+++ java/199.binary-tree-right-side-view.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=199 lang=java
+ *
+ * [199] Binary Tree Right Side View
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList();
+
+        dfs(root, 0, res);
+
+        return res;
+    }
+
+    public void dfs(TreeNode root, int level, List<Integer> res) {
+        if(root == null) return;
+        if(res.size() == level) {
+            res.add(root.val);
+        }
+
+        dfs(root.right, level + 1, res);
+        dfs(root.left, level + 1, res);
+    }
+}
+// @lc code=end
+
